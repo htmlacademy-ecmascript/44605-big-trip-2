53d53cb613bb12cb9141038b4c6af3045db485c1
Дмитрий Гилёв <kem-mit@yandex.ru>
2025-10-29 17:52:24 +0700

    Added the correct date format display

--- src/view/point-view.js
+++ src/view/point-view.js
@@ -32,16 +32,27 @@ function createPointComponent(point, destinations, offers) {
 
   function formatDuration(minutes) {
     const days = Math.floor(minutes / (24 * 60));
-    const remainingHours = minutes % (24 * 60);
-    const hours = Math.floor(remainingHours / 60);
-    const mins = remainingHours % 60;
+    const hours = Math.floor(minutes % (24 * 60) / 60);
+    const mins = minutes % 60;
+
+    // Альтернативный вариант, но есть проблема. Когда кол-во часов === 0, значение hours не попадает в массив и => в DOM
+    // const formattedDate = [];
+
+    // if (days > 0) {
+    //   formattedDate.push(`${days.toString().padStart(2, '0')}D`);
+    // } if (hours > 0) {
+    //   formattedDate.push(`${hours.toString().padStart(2, '0')}H`);
+    // }
+    // formattedDate.push(`${mins.toString().padStart(2, '0')}M`);
+
+    // return formattedDate.join(' ');
 
     if (days > 0) {
-      return `${days}D ${hours}H ${mins}M`;
+      return `${days.toString().padStart(2, '0')}D ${hours.toString().padStart(2, '0')}H ${mins.toString().padStart(2, '0')}M`;
     } else if (hours > 0) {
-      return `${hours}H ${mins}M`;
+      return `${hours.toString().padStart(2, '0')}H ${mins.toString().padStart(2, '0')}M`;
     } else {
-      return `${mins}M`;
+      return `${mins.toString().padStart(2, '0')}M`;
     }
   }
 
@@ -51,47 +62,47 @@ function createPointComponent(point, destinations, offers) {
   // Формирую кусок разметки
   // Если есть выбранные предложения для точки, то я рисую их, итерируясь по каждой точке с помощью map
   const offersHtml = selectedOffers.length > 0 ? `
-    <h4 class="visually-hidden">Offers:</h4>
-    <ul class="event__selected-offers">
-      ${selectedOffers.map((offer) => `
+      <h4 class="visually-hidden">Offers:</h4>
+        <ul class="event__selected-offers">
+          ${selectedOffers.map((offer) => `
         <li class="event__offer">
           <span class="event__offer-title">${offer.title}</span>
           &plus;&euro;&nbsp;
           <span class="event__offer-price">${offer.price}</span>
         </li>
       `).join('')}
-    </ul>
-  ` : '';
+        </ul>
+    ` : '';
 
   return `<li class="trip-events__item">
-              <div class="event">
-                <time class="event__date" datetime="${dateFrom}">${humanizeDate(dateFrom, DATE_FORMAT.dayMonth)}</time>
-                <div class="event__type">
-                  <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
-                </div>
-                <h3 class="event__title">${type} ${destinationName}</h3>
-                <div class="event__schedule">
-                  <p class="event__time">
-                    <time class="event__start-time" datetime="${dateFrom}">${timeStart}</time>
-                    &mdash;
-                    <time class="event__end-time" datetime="${dateTo}">${timeEnd}</time >
-                  </p >
-    <p class="event__duration">${formatDuration(durationTime)}</p>
-                </div >
-                <p class="event__price">
-                  &euro;&nbsp;<span class="event__price-value">${basePrice}</span>
-                </p>
-                ${offersHtml}
-                <button class="event__favorite-btn ${pointFavoritClassName}" type="button">
-                  <span class="visually-hidden">Add to favorite</span>
-                  <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
-                    <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
-                  </svg>
-                </button>
-                <button class="event__rollup-btn" type="button">
-                  <span class="visually-hidden">Open event</span>
-                </button>
-              </div >
+      <div class="event">
+        <time class="event__date" datetime="${dateFrom}">${humanizeDate(dateFrom, DATE_FORMAT.dayMonth)}</time>
+        <div class="event__type">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
+        </div>
+        <h3 class="event__title">${type} ${destinationName}</h3>
+        <div class="event__schedule">
+          <p class="event__time">
+            <time class="event__start-time" datetime="${dateFrom}">${timeStart}</time>
+            &mdash;
+            <time class="event__end-time" datetime="${dateTo}">${timeEnd}</time >
+          </p >
+          <p class="event__duration">${formatDuration(durationTime)}</p>
+        </div >
+        <p class="event__price">
+          &euro;&nbsp;<span class="event__price-value">${basePrice}</span>
+        </p>
+        ${offersHtml}
+        <button class="event__favorite-btn ${pointFavoritClassName}" type="button">
+          <span class="visually-hidden">Add to favorite</span>
+          <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
+            <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
+          </svg>
+        </button>
+        <button class="event__rollup-btn" type="button">
+          <span class="visually-hidden">Open event</span>
+        </button>
+      </div >
             </li > `;
 }
 /**
